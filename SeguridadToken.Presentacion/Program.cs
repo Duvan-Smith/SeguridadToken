e534ddaeece9e2546e1cc9f800afd6fe56f7e36d
DsmithMR <dsmith.mr@gmail.com>
2022-10-02 13:18:13 -0500

    Cambio run to IIS

--- SeguridadToken.Presentacion/Program.cs
+++ SeguridadToken.Presentacion/Program.cs
@@ -13,11 +13,11 @@ builder.Services.AddServerSideBlazor();
 
 builder.Services.AddHttpClient<ILoginService, LoginService>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7233/");
+    client.BaseAddress = new Uri("https://localhost:44376/");
 });
 builder.Services.AddHttpClient<IWeatherForecastService, WeatherForecastService>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7233/");
+    client.BaseAddress = new Uri("https://localhost:44376/");
 });
 
 builder.Services.AddBlazoredSessionStorage();
